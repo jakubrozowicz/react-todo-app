d517e580c2a47f7db75e9077118b7eb3b580e18e
Jakub Różowicz <jakub.rozowicz@corazlepszafirma.pl>
2023-02-23 13:26:38 +0100

    Updates

---
+++ precache-manifest.d2a43a36eee6072e137bbb3f8a9501e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7fc9c297bb0784eccd0",
+    "url": "/react-todo-app/static/css/main.1da3ae0f.chunk.css"
+  },
+  {
+    "revision": "c7fc9c297bb0784eccd0",
+    "url": "/react-todo-app/static/js/main.c7fc9c29.chunk.js"
+  },
+  {
+    "revision": "f6b84cc0c44b8101ab3f",
+    "url": "/react-todo-app/static/js/1.f6b84cc0.chunk.js"
+  },
+  {
+    "revision": "dc884f14d85a35ea3dce",
+    "url": "/react-todo-app/static/js/runtime~main.dc884f14.js"
+  },
+  {
+    "revision": "564e441dbe038399fdf3699177b1323d",
+    "url": "/react-todo-app/index.html"
+  }
+];
